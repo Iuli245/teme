6dd1db775216b26ef180de45926b036b37889a1f
Iuli245 <129730752+Iuli245@users.noreply.github.com>
2025-04-24 10:11:26 +0300

    Add files via upload

---
+++ test 2023/test 2023/FisaPacient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_2023
+{
+    [Serializable]
+    class FisaPacient : IContabil, ICloneable
+    {
+        private string Nume { get; set; }
+        private string Simptome { get; set; }
+        private int Durata_tratament { get; set; }
+        private List<Medicament> Medicamente { get; set; }
+
+        public FisaPacient(string n, string s, int d, List<Medicament> m)
+        {
+            Nume = n;
+            Simptome = s;
+            Durata_tratament = d;
+            Medicamente = m;
+        }
+        public double CalculPretTratament()
+        {
+            return Medicamente.Sum(m => m.CalculPretTratament());
+        }
+        public object Clone()
+        {
+            return new FisaPacient(
+                this.Nume,
+                this.Simptome,
+                this.Durata_tratament,
+                new List<Medicament>(this.Medicamente.Select(m => new Medicament(m.Denumire, m.Pret, m.Cantitate)))
+                );
+        }
+
+        public static FisaPacient operator +(FisaPacient f, Medicament m)
+        {
+            f.Medicamente.Add(m);
+            return f;
+        }
+
+    }
+}
---
+++ test 2023/test 2023/Medicament.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_2023
+{
+
+    [Serializable]
+   public class Medicament : IContabil
+    {
+        private const int Cod = 1;
+        private string Denumire {  get; set; }
+        private float Pret {  get; set; }
+        private float Cantitate {  get; set; }
+
+        public Medicament( string d, float p, float c)
+        {
+        
+            Denumire = d;
+            Pret = p;
+            Cantitate = c;
+        }
+       
+
+        public double CalculPretTratament()
+        {
+            return Pret * Cantitate;
+        }
+
+        public override string ToString()
+        {
+            return "Medicamentul " + Denumire + " are codul " + Cod + " cu pretul " + Pret + " si cantitate de " + Cantitate;
+
+        }
+      
+    }
+}
